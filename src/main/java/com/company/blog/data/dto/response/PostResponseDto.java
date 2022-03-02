eeceacfd746eae29c07b51a5bcd8c387a4b874bd
anar_mammadov <anar.memmedov@expressbank.az>
2022-03-02 12:06:36 +0400

    In this branch was changing  POST GET By ID web service.

--- src/main/java/com/company/blog/data/dto/response/PostResponseDto.java
+++ src/main/java/com/company/blog/data/dto/response/PostResponseDto.java
@@ -1,6 +1,9 @@
 package com.company.blog.data.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
+
+import java.util.Set;
 
 @Data
 public class PostResponseDto {
@@ -8,4 +11,6 @@ public class PostResponseDto {
     private String content;
     private String title;
     private String description;
+//    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Set<CommentResponseDto> comments;
 }
