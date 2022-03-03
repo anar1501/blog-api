9d2d5972e7734a0d815c6b018df61ce1954079bd
Anar <71570209+anar1996@users.noreply.github.com>
2022-03-03 11:06:25 +0400

    In this branch I was creating Validation for Comment web services.

--- src/main/java/com/company/blog/controller/CommentController.java
+++ src/main/java/com/company/blog/controller/CommentController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -16,7 +18,7 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("/posts/{postId}/comments")
-    public ResponseEntity<CommentResponseDto> createComment(@PathVariable("postId") Long postId, CommentRequestDto commentRequestDto) {
+    public ResponseEntity<CommentResponseDto> createComment(@PathVariable("postId") Long postId,@Valid @RequestBody CommentRequestDto commentRequestDto) {
         return new ResponseEntity<>(commentService.createComment(postId, commentRequestDto), HttpStatus.CREATED);
     }
 
@@ -31,7 +33,7 @@ public class CommentController {
     }
 
     @PutMapping("/posts/{postId}/comments/{commentId}")
-    public ResponseEntity<CommentResponseDto> updateComment(@PathVariable("postId") Long postId, @PathVariable("commentId") Long commentId, @RequestBody CommentRequestDto requestDto) {
+    public ResponseEntity<CommentResponseDto> updateComment(@PathVariable("postId") Long postId, @PathVariable("commentId") Long commentId,@Valid @RequestBody CommentRequestDto requestDto) {
         return ResponseEntity.ok(commentService.updateComment(postId, commentId, requestDto));
     }
 
--- src/main/java/com/company/blog/exception/PostExceptionHandler.java
+++ src/main/java/com/company/blog/exception/PostExceptionHandler.java
@@ -2,16 +2,23 @@ package com.company.blog.exception;
 
 import com.company.blog.enums.ErrorCase;
 import com.company.blog.resource.ErrorResponse;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
-public class PostExceptionHandler {
+public class PostExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
@@ -30,4 +37,29 @@ public class PostExceptionHandler {
         errorResponse.setMessage(ErrorCase.NOT_FOUND.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                               HttpHeaders headers,
+                                                               HttpStatus status,
+                                                               WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGlobalException(Exception exception,
+                                                               WebRequest webRequest) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setCode(500);
+        errorResponse.setMessage(exception.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
