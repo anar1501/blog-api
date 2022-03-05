0ce4ef053f399f4165d1f4a5130c8cc10c53ceb2
Anar <71570209+anar1996@users.noreply.github.com>
2022-03-05 16:04:26 +0400

    In this branch was creating User and Role entities and make relationships between them with ManyToMany

---
+++ src/main/java/com/company/blog/data/entity/User.java
@@ -0,0 +1,26 @@
+package com.company.blog.data.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler", "roles"})
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    @Column(unique = true)
+    private String email;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+}
