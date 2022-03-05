857b396b03743e627d80463bc397474e09f5d29a
Anar <71570209+anar1996@users.noreply.github.com>
2022-03-05 16:05:37 +0400

    adding cascade type All to the User Entity

--- src/main/java/com/company/blog/data/entity/User.java
+++ src/main/java/com/company/blog/data/entity/User.java
@@ -20,7 +20,7 @@ public class User {
     private String password;
     @Column(unique = true)
     private String email;
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
     @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
 }
